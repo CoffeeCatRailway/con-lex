90053f31a8201217c1084a2570a3d03c8d2cf839
CoffeeCatRailway <dragonlee1002@gmail.com>
2026-09-16 10:19:46 +1000

    Fix  trying to load font on open when path is empty

--- scripts/save_data.gd
+++ scripts/save_data.gd
@@ -70,7 +70,7 @@ func loadFrom(path: String, conLex: ConLex, webBuild: bool, webData: String = ""
 	self.useWrittenFont = json.data["useWrittenFont"]
 	self.writtenFontPath = json.data["writtenFontPath"]
 	GlobalVars.useWrittenFont = self.useWrittenFont
-	if !webBuild: # Don't load fonts on web
+	if !webBuild && self.useWrittenFont: # Don't load fonts on web
 		GlobalVars.writtenFont = GlobalVars.loadFont(self.writtenFontPath)
 		conLex.useFontBtn.button_pressed = self.useWrittenFont
 	
